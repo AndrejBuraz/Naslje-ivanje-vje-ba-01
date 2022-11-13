c676e3e0b55402850d64df404c032872b735a5e8
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-11-13 16:25:24 +0100

    Add files via upload

---
+++ NasljedivanjeAndrej01/NasljedivanjeAndrej01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasljedivanjeAndrej01
+{
+
+    class Cat
+    {
+        public bool opasna;
+
+        public virtual void Opasnost()
+        {
+            this.opasna = false;
+        }
+        
+    }
+    class Gepard : Cat
+    {
+        public override void Opasnost()
+        {
+            this.opasna = true;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.Opasnost();
+            Gepard g = new Gepard();
+            g.Opasnost();
+            Console.WriteLine("Gepard opasna: "+ g.opasna + " \nMačka opasna: " + c.opasna);
+            Console.ReadKey();
+        }
+    }
+}
